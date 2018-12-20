91561494bb2155784b262008931218d8deb0298f
Jonsonice <415410795@qq.com>
2018-12-20 22:58:14 +0800

    实现翻译

--- online-translation/src/App.vue
+++ online-translation/src/App.vue
@@ -1,38 +1,48 @@
 <template>
-  <div id="app">
+	<div id="app">
 		<h1>在线翻译</h1>
 		<h5>简单 / 易用 / 便捷</h5>
 		<translateForm v-on:formSubmits="translateText"></translateForm>
-  </div>
+		<translateOutput v-text="translatedText">TranslateOutput</translateOutput>
+	</div>
 </template>
 
 <script>
-import TranslateForm from './components/TranslateForm'
+	import TranslateForm from './components/TranslateForm'
+	import TranslateOutput from './components/TranslateOutput'
 
-export default {
-  name: 'App',
-  components: {
-    TranslateForm
-  },
-	methods:{
-		translateText:function(text){
-			// alert(text);
-			this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181220T142434Z.bd4bff0edd6e54be.564cbaddc02ba4ade4b1a35d33a70bc3099ea318&lang=en&text='+text)
-			.then((response)=>{
-				console.log(response.body.text[0]);
-			})
+	export default {
+		name: 'app',
+		data: function() {
+			return {
+				translatedText: ""
+			}
+		},
+		components: {
+			TranslateForm,TranslateOutput
+		},
+		methods: {
+			translateText: function(text,language) {
+				// alert(text);
+				this.$http.get(
+						'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181220T142434Z.bd4bff0edd6e54be.564cbaddc02ba4ade4b1a35d33a70bc3099ea318&lang='+language+'&text=' +
+						text)
+					.then((response) => {
+						// console.log(response.body.text[0]);
+						this.translatedText = response.body.text[0]
+					})
+			}
 		}
 	}
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- online-translation/src/components/TranslateForm.vue
+++ online-translation/src/components/TranslateForm.vue
@@ -2,8 +2,12 @@
   <div id="translateForm">
 	<form v-on:submit="formSubmit">
 		<input type="text" placeholder="输入需要翻译的内容" v-model="textToTranslate">
-		<select>
+		<select v-model="language">
 			<option value="en">English</option>
+			<option value="ru">Russian</option>
+			<option value="ko">Korean</option>
+			<option value="ja">Janpenese</option>
+			
 		</select>
 		<input type="submit" value="翻译">
 	</form>
@@ -16,13 +20,14 @@ export default {
   name: 'translateForm',
   data:function(){
 	return{
-		textToTranslate:""
+		textToTranslate:"",
+		language:""
 	}  
   },
   methods:{
 	  formSubmit:function(e){
 		  // alert(this.textToTranslate);
-		  this.$emit("formSubmits",this.textToTranslate);
+		  this.$emit("formSubmits",this.textToTranslate,this.language);
 		  e.preventDefault();//取消默认事件 刷新页面
 	  }
   }
